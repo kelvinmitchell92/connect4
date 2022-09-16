ff3c0e17eb20a87e8c313ed1f63c3329f255e2d9
Kelvin Mitchell <kelvin.mitchell@doane.edu>
2022-09-16 10:01:11 -0500

    Add project files.

---
+++ Connect4/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace connect4
+{
+
+	public class Board
+	{
+		public int[,] board = new int[6,7];
+
+		public Board()
+		{
+
+			for(int i = 0; i < 6; i++)
+            {
+				for(int j = 0; j < 7; j++)
+                {
+					board[i,j] = 0;
+                }
+            }
+
+		}
+
+		public void boardToString(Board board)
+        {
+			for (int i = 0; i < 6; i++)
+			{
+
+				Console.WriteLine(board.board[i, 0] + " " + board.board[i, 1] + " " + board.board[i, 2] + " " + board.board[i, 3] + " " + board.board[i, 4] + " " + board.board[i, 5] + " " + board.board[i, 6]);
+				
+			}
+
+		}
+	}
+}
---
+++ Connect4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace connect4
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Board board1 = new Board();
+            board1.boardToString(board1);
+
+        }
+
+    }
+}
